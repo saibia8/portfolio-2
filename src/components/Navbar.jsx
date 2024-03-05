df9fd6cdb934b31cc57c26bd64bc84f1d0f23efa
saibia8 <sabina.kutniauske@gmail.com>
2024-03-05 20:43:09 +0100

    Started creating pages and navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <div className='h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
+      {/* LOGO */}
+      <div>1</div>
+      {/* RESPONSIVE MENU */}
+      <div>2</div>
+    </div>
+  );
+};
+
+export default Navbar;
