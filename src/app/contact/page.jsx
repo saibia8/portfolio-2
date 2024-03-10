e4500133273c2c182bc58efb45a10b4889b4193f
saibia8 <sabina.kutniauske@gmail.com>
2024-03-10 21:18:57 +0100

    ok

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -45,7 +45,7 @@ const ContactPage = () => {
     >
       <div className='h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
         {/* TEXT CONTAINER */}
-        <div className='h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center text-4xl mr-5'>
+        <div className='h-1/4 lg:h-full lg:w-1/2 flex items-center justify-center text-4xl mr-5'>
           <motion.div>
             {text.split('').map((letter, index) => (
               <motion.span
@@ -67,7 +67,7 @@ const ContactPage = () => {
         <form
           onSubmit={sendEmail}
           ref={form}
-          className='h-1/2 lg:h-full lg:w-1/2 bg-[#f5f0e2] rounded-2xl text-xl flex flex-col gap-8 justify-center p-24'
+          className='h-3/4 lg:h-full lg:w-1/2 bg-[#f5f0e2] rounded-2xl text-xl flex flex-col gap-8 justify-center p-24'
         >
           <span>Your message:</span>
           <textarea
